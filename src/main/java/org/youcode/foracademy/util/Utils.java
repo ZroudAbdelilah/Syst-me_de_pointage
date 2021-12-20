adc5f789a725efb89d8678dd4d8da51a072d8842
ZroudAbdelilah <76938527+ZroudAbdelilah@users.noreply.github.com>
2021-12-20 02:15:48 +0100

    Creation Utils aide nous de changer Date from java utils to Date java.sql

---
+++ src/main/java/org/youcode/foracademy/util/Utils.java
@@ -0,0 +1,7 @@
+package org.youcode.foracademy.util;
+
+public class Utils {
+    public static java.sql.Date getSqlDate(java.util.Date utilDate) {
+        return new java.sql.Date(utilDate.getTime());
+    }
+}

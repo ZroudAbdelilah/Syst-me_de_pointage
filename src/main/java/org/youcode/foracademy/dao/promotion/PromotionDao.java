56f414c0a91b501b605628a51988c504f1a82ad6
ZroudAbdelilah <76938527+ZroudAbdelilah@users.noreply.github.com>
2021-12-21 16:23:41 +0100

    Creation interface PromotionDao

---
+++ src/main/java/org/youcode/foracademy/dao/promotion/PromotionDao.java
@@ -0,0 +1,13 @@
+package org.youcode.foracademy.dao.promotion;
+
+import org.youcode.foracademy.models.Promotion;
+
+import java.util.List;
+
+public interface PromotionDao {
+    public long delete(long id);
+    public List<Promotion> findAll();
+    public Promotion findById(long id);
+    public void insert(Promotion promotion);
+    public void update(Promotion promotion);
+}

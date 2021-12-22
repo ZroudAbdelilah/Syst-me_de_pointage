3abac888dfde73b56a570949eaeea77fa7ecbc06
ZroudAbdelilah <76938527+ZroudAbdelilah@users.noreply.github.com>
2021-12-22 14:54:34 +0100

    modification models Formateur

--- src/main/java/org/youcode/foracademy/models/Formateur.java
+++ src/main/java/org/youcode/foracademy/models/Formateur.java
@@ -1,15 +1,15 @@
 package org.youcode.foracademy.models;
 
 public class Formateur extends User {
+    private Class aclass;
     private Specialiter specialiter;
 
-    public Formateur(Specialiter specialiter) {
-        this.specialiter = specialiter;
+    public Class getAclass() {
+        return aclass;
     }
 
-    public Formateur(long id_user, String first_name, String last_name, String email, String password, long phone, String gander, boolean status_compte, Role role, Adress adress, Fabrique fabrique, Specialiter specialiter) {
-        super(id_user, first_name, last_name, email, password, phone, gander, status_compte, role, adress,  fabrique);
-        this.specialiter = specialiter;
+    public void setAclass(Class aclass) {
+        this.aclass = aclass;
     }
 
     public Specialiter getSpecialiter() {
@@ -20,10 +20,14 @@ public class Formateur extends User {
         this.specialiter = specialiter;
     }
 
-    @Override
-    public String toString() {
-        return "Formateur{" +
-                "specialiter=" + specialiter +
-                '}';
+    public Formateur(Class aclass, Specialiter specialiter) {
+        this.aclass = aclass;
+        this.specialiter = specialiter;
+    }
+
+    public Formateur(long id_user, String first_name, String last_name, String email, String password, long phone, String gander, boolean status_compte, Role role, Adress adress, Fabrique fabrique, Class aclass, Specialiter specialiter) {
+        super(id_user, first_name, last_name, email, password, phone, gander, status_compte, role, adress, fabrique);
+        this.aclass = aclass;
+        this.specialiter = specialiter;
     }
 }

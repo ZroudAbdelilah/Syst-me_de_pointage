a6116f56d59e69704608b4855a689653830f452f
JAMAL EDDINE ELIDRISSI Yassir <67668097+JAMALEEY@users.noreply.github.com>
2021-12-29 11:52:54 +0100

    add excpetions

---
+++ src/main/java/org/youcode/foracademy/dao/DAOException/DAOConfigurationException.java
@@ -0,0 +1,15 @@
+package org.youcode.foracademy.dao.DAOException;
+
+public class DAOConfigurationException extends RuntimeException {
+    public DAOConfigurationException(String message) {
+        super(message);
+    }
+
+    public DAOConfigurationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DAOConfigurationException(Throwable cause) {
+        super(cause);
+    }
+}

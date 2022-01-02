67e597392d3c95db0f8eb67f73a252fea094ff52
ZroudAbdelilah <76938527+ZroudAbdelilah@users.noreply.github.com>
2022-01-02 02:42:43 +0100

    refactoring code

--- src/test/java/org/youcode/foracademy/services/FormateurServiceTest.java
+++ src/test/java/org/youcode/foracademy/services/FormateurServiceTest.java
@@ -8,56 +8,7 @@ import java.sql.Date;
 import java.util.LinkedList;
 
 import static org.junit.jupiter.api.Assertions.*;
-/*
 
-
-    @Test
-     void findById() {
-
-        ClassService classService = new ClassService();
-        assertInstanceOf(Class.class,classService.findById(1));
-    }
-
-    @Test
-    void findAll() {
-
-        ClassService classService = new ClassService();
-        LinkedList<Class> classes = new LinkedList<Class>();
-        assertInstanceOf(classes.getClass(),classService.findAll());
-    }
-
-    @Test
-    void delete() {
-
-        ClassService classService = new ClassService();
-
-        assertTrue(classService.delete(2));
-    }
-
-    @Test
-    void insert() {
-        ClassService classService = new ClassService();
-        PromotionService promotionService=new PromotionService();
-        Promotion promotion = new Promotion(5,"helloooforClass",new Date(11,11,11),new Date(12,12,11),new Date(10,5,22));
-        assertInstanceOf(Promotion.class,promotionService.insert(promotion));
-
-        Class aclass = new Class(0,"java",new Date(11,11,11),new Date(111,11,13),promotion);
-
-        assertInstanceOf(Class.class,classService.insert(aclass));
-    }
-
-    @Test
-    void update() {
-        ClassService classService = new ClassService();
-        PromotionService promotionService=new PromotionService();
-        Promotion promotion = new Promotion(5,"helloooforClass",new Date(11,11,11),new Date(12,12,11),new Date(10,5,22));
-        assertInstanceOf(Promotion.class,promotionService.insert(promotion));
-
-        Class aclass = new Class(1,"javatestupdate",new Date(11,11,11),new Date(111,11,13),promotion);
-
-        assertInstanceOf(Class.class,classService.update(aclass));
-    }
- */
 class FormateurServiceTest {
 
     @Test
